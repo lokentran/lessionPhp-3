4fca802af33bc1dab326a7a64c62d0786c865049
lokentran <thanhtungmarine@gmail.com>
2020-06-23 23:28:15 +0700

    Learn construct

---
+++ class.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+<?php
+
+class Fruit {
+    public $name;
+    public $color;
+
+    function __construct($name) {
+        $this->name = $name;
+    }
+
+    function get_name() {
+        return $this->name;
+      }
+
+
+}
+
+$apple = new Fruit('Apple');
+
+echo $apple->get_name();
+
+
+?>
+
+
+
+</body>
+</html>
